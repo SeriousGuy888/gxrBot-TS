921545db243b4dcedaf32f23562d7945016448e3
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-04-13 01:00:57 -0400

    ported over billzonian dictionary command
    made just a tiny bit more readable (cognitive complexity lowered from 48 to 25)

---
+++ src/commands/Billzonian.ts
@@ -0,0 +1,275 @@
+import { SlashCommandBuilder } from "@discordjs/builders"
+import {
+  CommandInteraction,
+  Message,
+  MessageActionRow,
+  MessageButton,
+  MessageComponentInteraction,
+  MessageEmbed
+} from "discord.js"
+import axios from "axios"
+import csv from "csvtojson"
+
+const repoUrl = "https://github.com/SeriousGuy888/Billzonian"
+const dictionaryUrl = "https://seriousguy888.github.io/Billzonian/vocabulary.csv"
+
+
+export const data = new SlashCommandBuilder()
+  .setName("billzonian")
+  .setDescription("Look up a word in the Billzonian dictionary")
+  .addStringOption(option => option
+    .setName("word")
+    .setDescription("Search term - Billzonian or English word"))
+
+export async function execute(interaction: CommandInteraction) {
+  let response
+  try {
+    response = await axios.get(dictionaryUrl)
+  } catch (error) {
+    interaction.followUp({ content: "Could not fetch dictionary data." })
+    return
+  }
+  
+  const dictionaryData = await csv().fromString(response.data)
+  const itemsPerPage = 4
+  const searchTerm = interaction.options.getString("word")?.toLowerCase()
+  let maxPages = Math.ceil(dictionaryData.length / itemsPerPage)
+  let page = 1
+  let msg = (await interaction.followUp({ content: "Loading dictionary..." })) as Message
+
+  const displayDictionary = async (targetMessage: Message, disableButtons: boolean) => {
+    let searchResults = dictionaryData.filter(e => (
+      e.word.toLowerCase().includes(searchTerm)
+      || e.translation.toLowerCase().includes(searchTerm)
+      || e.alt_forms.toLowerCase().includes(searchTerm)
+    ))
+
+    maxPages = Math.ceil(searchResults.length / itemsPerPage)
+
+
+
+    const responseEmbed = new MessageEmbed()
+      .setColor("#fca503")
+      .setTitle("The Billzonian-English Dictionary")
+      .setURL(dictionaryUrl)
+      .setFooter({ text: `Page ${page} of ${maxPages}` })
+      .setDescription([
+        "This dictionary is not necessarily a comprehensive collection.",
+        `If a word is missing, you can [make an issue here.](${repoUrl})`,
+        "",
+        `:mag: Search Term: \`${searchTerm || "[None]"}\``,
+        "\u200b",
+      ].join("\n"))
+    
+
+
+    if(maxPages === 0) {
+      // const allWords = dictionaryData.map(e => e.word)
+      // let allSimilarities = didYouMean(searchTerm, allWords).map(e => e.target)
+      // allSimilarities = [...new Set(allSimilarities)] // remove duplicates from function return
+      // const topSimilarities = []
+
+      // for(let i = 0; i < 8; i++) {
+      //   topSimilarities.push(allSimilarities[i])
+      // }
+
+      // const wordSuggestionsStr = "```\n" + topSimilarities.map(e => `- ${e}`).join("\n") + "```"
+
+
+      responseEmbed
+        .addField("No Words Found :(", [
+          "Try double checking your search term.",
+          "Shu attempt tu reakratise thy search term.",
+        ].join("\n"))
+        .addField("\u200b", "\u200b")
+        // .addField("Did you mean one of these words?", wordSuggestionsStr.slice(0, 1024))
+    } else {
+      searchResults.forEach(e => {
+        if(e?.word?.toLowerCase() === searchTerm) {
+          e.isExactMatch = true
+          searchResults = moveArrayItem(searchResults, searchResults.indexOf(e), 0)
+        }
+      })
+
+
+      for(let i = 0; i < itemsPerPage; i++) {
+        const entryIndex = i + ((page - 1) * itemsPerPage)
+        const wordData = searchResults[entryIndex]
+        if(!wordData)
+          break
+
+        addWordToEmbed(wordData, responseEmbed)
+        
+        if((i + 1) % 2 === 0)
+          responseEmbed.addField("\u200b", "\u200b")
+      }
+    }
+    responseEmbed.addField("\u200b", "\u200b")
+
+
+    const buttonRows = [
+      new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId("first")
+          .setLabel("First")
+          .setStyle("SECONDARY")
+          .setDisabled(page <= 1),
+        new MessageButton()
+          .setCustomId("prev")
+          .setLabel("<")
+          .setStyle("PRIMARY")
+          .setDisabled(page <= 1),
+        new MessageButton()
+          .setCustomId("next")
+          .setLabel(">")
+          .setStyle("PRIMARY")
+          .setDisabled(page >= maxPages),
+        new MessageButton()
+          .setCustomId("last")
+          .setLabel("Last")
+          .setStyle("SECONDARY")
+          .setDisabled(page >= maxPages),
+      ),
+      new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId("prev10")
+          .setLabel("< 10")
+          .setStyle("SECONDARY")
+          .setDisabled(page <= 10),
+        new MessageButton()
+          .setCustomId("prev3")
+          .setLabel("< 3")
+          .setStyle("SECONDARY")
+          .setDisabled(page <= 3),
+        new MessageButton()
+          .setCustomId("next3")
+          .setLabel("3 >")
+          .setStyle("SECONDARY")
+          .setDisabled(page >= maxPages - 3),
+        new MessageButton()
+          .setCustomId("next10")
+          .setLabel("10 >")
+          .setStyle("SECONDARY")
+          .setDisabled(page >= maxPages - 10),
+      )
+    ]
+    if(disableButtons) {
+      buttonRows.forEach(row => {
+        row.components.forEach(comp => comp.setDisabled(true))
+      })
+    }
+
+    return targetMessage.edit({
+      content: null,
+      embeds: [responseEmbed],
+      components: buttonRows,
+    })
+  }
+
+  msg = await displayDictionary(msg, false)
+
+  const filter = (inter: MessageComponentInteraction) => inter.user.id === interaction.user.id
+  const collector = msg.channel.createMessageComponentCollector({ filter, time: 60000 })
+    .on("collect", async (inter) => {
+      collector.resetTimer()
+
+      const buttonId = inter.customId
+      switch(buttonId) {
+        case "first":
+          page = 1
+          break
+        case "prev10":
+          page -= 10
+          break
+        case "prev3":
+          page -= 3
+          break
+        case "prev":
+          page--
+          break
+        case "next":
+          page++
+          break
+        case "next3":
+          page += 3
+          break
+        case "next10":
+          page += 10
+          break
+        case "last":
+          page = maxPages
+          break
+      }
+
+      page = Math.max(Math.min(maxPages, page), 1)
+
+      inter.deferUpdate().catch(() => { /* swallow error */ })
+      displayDictionary(msg, false)
+    })
+    .on("end", async () => {
+      // if the timer expired, edit message and disable all buttons
+      displayDictionary(msg, true)
+    })
+}
+
+
+const addWordToEmbed = (wordData: any, embed: MessageEmbed) => {
+  const ipaReadings = wordData.ipa.split("|")
+  const alts = wordData.alt_forms.split("|")
+  const translation = wordData.translation
+  const example = wordData.example
+  const notes = wordData.notes
+
+  let ipaReadingsString = "No IPA transcription provided."
+  if(wordData.ipa) {
+    ipaReadingsString = ipaReadings
+      .map((e: string) => `/[${e}](http://ipa-reader.xyz/?text=${encodeURI(e)})/`)
+      .join(" or ")
+  }
+
+  embed.addField(
+    `${wordData.word && "**" + wordData.word + "**"} \`${wordData.pos}\` ${wordData.isExactMatch ? "*(⭐ Exact Match)*" : ""}`,
+    [
+      ipaReadingsString,
+      wordData.alt_forms && `\`Alt:\` ${alts.join(", ")}`,
+      listify(translation, "numbers"),
+      listify(example, "letters"),
+      listify(notes, "bullets"),
+    ].filter(e => e).join("\n"),
+    true
+  )
+}
+
+
+const listify = (str: string, startLineWith: ("numbers" | "letters" | "bullets")) => {
+  if(!str)
+    return ""
+
+  const lines = str.split("|")
+  const numberedLines = []
+  const letters = "abcdefghijklmnopqrstuvwxyz"
+  for(let i = 0; i < lines.length; i++) {
+    let bullet
+    switch(startLineWith) {
+      case "numbers":
+        bullet = `\`${(i + 1).toString() + "."}\``
+        break
+      case "letters":
+        bullet = "`" + letters.charAt(i % letters.length) + ".`"
+        break
+      case "bullets":
+        bullet = "\`•\`"
+        break
+    }
+
+    numberedLines.push(`${bullet} ${lines[i]}`)
+  }
+
+  return numberedLines.join("\n")
+}
+
+const moveArrayItem = (array: any[], fromIndex: number, toIndex: number) => {
+  const arr = [...array]
+  arr.splice(toIndex, 0, ...arr.splice(fromIndex, 1))
+  return arr
+}
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,2 +1,3 @@
+export * as billzonian from "./Billzonian"
 export * as ping from "./Ping"
 export * as test from "./Test"
