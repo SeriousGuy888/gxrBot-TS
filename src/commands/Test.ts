4dd2ee0234fc165416300ef17659053a2bd1e579
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-04-15 00:24:10 -0400

    test command modifies user data now

--- src/commands/Test.ts
+++ src/commands/Test.ts
@@ -1,21 +1,26 @@
 import { SlashCommandBuilder } from "@discordjs/builders"
 import { CommandInteraction } from "discord.js"
-import { Command } from "src/interfaces"
-import { createDocument } from "../firebase"
+import { getUserData, setUserData, writeUsersToFirestore } from "../firebase/usersColl"
+import { Command } from "../interfaces"
 
 const data = new SlashCommandBuilder()
   .setName("test")
   .setDescription("test")
-  .addStringOption(option => {
+  .addUserOption(option => {
     return option
-      .setName("text")
-      .setDescription("aaaa")
+      .setName("user")
+      .setDescription("user")
       .setRequired(true)
   })
 
 async function execute(interaction: CommandInteraction) {
-  await createDocument(interaction.user.id, interaction.options.getString("text")!)
-  await interaction.followUp({ content: "ok", ephemeral: true })
+  const user = interaction.options.getUser("user")!
+  let userData = await getUserData(user.id)
+  userData.karma ??= 0
+  userData.karma++
+
+  await setUserData(user.id, userData)
+  await interaction.followUp({ content: `added 1 karma to ${user.tag}` })
 }
 
 export const Test = { data, execute } as Command
