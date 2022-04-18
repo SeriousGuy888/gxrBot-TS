b8e8ff1ae7c177f696a7171f7186a527f66b0d5e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-04-18 16:28:06 -0400

    graceful shutdown for writing karma changes

--- src/bot.ts
+++ src/bot.ts
@@ -4,6 +4,8 @@ import Client from "./ExtendedClient"
 import deployCommands from "./deployCommands"
 import deployEvents from "./deployEvents"
 import "./firebase/firebase"
+import { writeKarmaChanges } from "./firebase/karmaDb"
+import { onShutdown } from "node-graceful-shutdown"
 
 export const client = new Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
@@ -19,3 +21,10 @@ client.once("ready", async () => {
 })
 
 client.login(process.env.BOT_TOKEN)
+
+
+onShutdown(async () => {
+  console.log("Graceful shutdown in progress...")
+  await writeKarmaChanges()
+  console.log("Ready to shutdown.")
+})
