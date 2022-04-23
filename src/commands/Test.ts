8f67a38813b43353d6347bac7853a36e5d164a22
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-04-23 16:40:40 -0400

    Update Test.ts

--- src/commands/Test.ts
+++ src/commands/Test.ts
@@ -11,6 +11,11 @@ const data = new SlashCommandBuilder()
   })
 
 async function execute(interaction: CommandInteraction) {
+  if (interaction.user.id !== "323170410818437130") {
+    interaction.followUp("le non")
+    return
+  }
+
   await writeKarmaChanges()
   await interaction.followUp({ content: "ok" })
 }
