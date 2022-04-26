bde7c666a8da320edf753eea1328d249c74e2ca7
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-04-25 22:06:02 -0400

    some attempt to do an autorestart on replit

--- src/keepAlive.ts
+++ src/keepAlive.ts
@@ -1,6 +1,31 @@
-import http from "http"
+import readline from "node:readline"
+import express from "express"
+import { stdin } from "process"
+const app = express()
 
-http.createServer((req, res) => {
-  res.write("HTTP server running.")
+app.get("/", (req, res) => {
+  res.send("HTTP server running")
   res.end()
-}).listen(8080)
+})
+
+app.post("/refresh", async (req, res) => {
+  console.log("repl.deploy" + req.body + req.header("Signature"))
+
+  const result: {
+    body: string
+    status: number
+  } = JSON.parse((await getStdinLine())!)
+
+  res.sendStatus(result.status).end(result.body)
+  console.log("repl.deploy-success")
+})
+
+app.listen(8080, () => {
+  console.log("Started HTTP server")
+})
+
+async function getStdinLine() {
+  const chunks = []
+  for await (const chunk of stdin) chunks.push(chunk)
+  return Buffer.concat(chunks).toString("utf8")
+}
