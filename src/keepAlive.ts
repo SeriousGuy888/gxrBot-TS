9fe118abe57b63fe713a0cfb39552d3a1c58fec5
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-04-25 22:25:47 -0400

    test replit autoredeploy

--- src/keepAlive.ts
+++ src/keepAlive.ts
@@ -1,5 +1,4 @@
 import express from "express"
-import { stdin } from "process"
 const app = express()
 
 app.get("/", (_, res) => {
@@ -7,24 +6,6 @@ app.get("/", (_, res) => {
   res.end()
 })
 
-app.post("/refresh", async (req, res) => {
-  console.log("repl.deploy" + req.body + req.header("Signature"))
-
-  const result: {
-    body: string
-    status: number
-  } = JSON.parse((await getStdinLine())!)
-
-  res.sendStatus(result.status).end(result.body)
-  console.log("repl.deploy-success")
-})
-
 app.listen(8080, () => {
   console.log("Started HTTP server")
 })
-
-async function getStdinLine() {
-  const chunks = []
-  for await (const chunk of stdin) chunks.push(chunk)
-  return Buffer.concat(chunks).toString("utf8")
-}
