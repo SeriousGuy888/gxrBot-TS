791ee1ce3dbfbadf5f3201e4e249d8916924b1d9
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-12-27 21:22:24 -0500

    rewrite formatAsList to work with string arrays

--- src/util/stringFormatter.ts
+++ src/util/stringFormatter.ts
@@ -1,29 +1,32 @@
 const letters = "abcdefghijklmnopqrstuvwxyz"
 
 export function formatAsList(
-  multilineString: string,
+  lines: string[],
   startLineWith: "numbers" | "letters" | "bullets",
 ) {
-  if (!multilineString.trim()) {
-    return ""
-  }
-  
-  const lines = multilineString.split("\n")
-
   if (startLineWith === "bullets") {
-    return lines.map((line) => "`•` " + line)
+    return lines
+      .map((line) => "`•` " + line)
+      .join("\n")
+      .trim()
   }
 
   let currIndex = 0
   if (startLineWith === "numbers") {
-    return lines.map((line) => {
-      currIndex++
-      return "`" + currIndex + ".` " + line
-    })
+    return lines
+      .map((line) => {
+        currIndex++
+        return "`" + currIndex + ".` " + line
+      })
+      .join("\n")
+      .trim()
   } else {
-    return lines.map((line) => {
-      currIndex++
-      return "`" + letters.charAt(currIndex % letters.length) + ".` " + line
-    })
+    return lines
+      .map((line) => {
+        currIndex++
+        return "`" + letters.charAt(currIndex % letters.length) + ".` " + line
+      })
+      .join("\n")
+      .trim()
   }
 }
