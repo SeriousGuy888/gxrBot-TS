6dae4a30e9fe6eebb4d43f74de03a7d518c18d06
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-18 00:23:02 -0400

    fix intents to receive message content

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,7 @@
-import "./config"             // Set up and make sure all the env variables are there
-import "./firebase/firebase"  // Connect to Firebase, and start updating karma
+import "./config" // Set up and make sure all the env variables are there
+import "./firebase/firebase" // Connect to Firebase, and start updating karma
 
-import cron from "node-cron"    
+import cron from "node-cron"
 import express from "express"
 import { IntentsBitField, Partials } from "discord.js"
 import { ActivityType } from "discord-api-types/v10"
@@ -14,14 +14,9 @@ import deployEvents from "./deployEvents"
 import { writeKarmaChanges } from "./firebase/karmaDb"
 import { sendBirthdayReminders } from "./util/birthdayReminder"
 
-
 export const client = new Client({
-  partials: [
-    Partials.Message,
-    Partials.Channel,
-    Partials.Reaction,
-  ],
-  intents: new IntentsBitField(32767),
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+  intents: new IntentsBitField(131071), // https://ziad87.net/intents/
 })
 
 client.once("ready", async () => {
@@ -39,9 +34,13 @@ client.once("ready", async () => {
   await deployCommands()
   await deployEvents()
 
-  cron.schedule("0 0 * * *", async (date) => {
-    await sendBirthdayReminders(date)
-  }, { timezone: "America/Toronto" })
+  cron.schedule(
+    "0 0 * * *",
+    async (date) => {
+      await sendBirthdayReminders(date)
+    },
+    { timezone: "America/Toronto" },
+  )
 
   console.log(`Authenticated as ${client.user.tag}`)
 })
@@ -53,12 +52,9 @@ app.get("/", (req, res) => {
   res.send("Hello, world!")
 })
 app.get("/ping", (req, res) => {
-  res
-    .status(200)
-    .send("gxrbot online!")
+  res.status(200).send("gxrbot online!")
 })
 app.listen(10000, () => console.log("Listening for HTTP requests!"))
-
 
 onShutdown(async () => {
   console.log("Graceful shutdown in progress...")
